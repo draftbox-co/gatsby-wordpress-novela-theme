4d0c14001dbadcd36220edcbce8edcd3c1201057
Keyur <keyur.raval18@gmail.com>
2020-07-13 15:54:30 +0530

    Adds contact meta support

--- gatsby-wordpress-novela-theme/src/pages/contact.tsx
+++ gatsby-wordpress-novela-theme/src/pages/contact.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
 import Layout from "../components/Layout";
+import ContactMeta from "@components/meta/contact-meta";
 
-const Contact = () => {
+const Contact = ({location}) => {
   return (
     <Layout>
+      <ContactMeta location={location}/>
       <ContactForm />
     </Layout>
   );
