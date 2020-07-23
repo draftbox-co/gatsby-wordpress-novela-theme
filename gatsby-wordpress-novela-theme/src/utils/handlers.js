041607d6e2923c5ee259784f549a17e6b41e0086
Shyam <shyamlohar1996@gmail.com>
2020-07-23 15:05:26 +0530

    Added support for audio and video elements

--- gatsby-wordpress-novela-theme/src/utils/handlers.js
+++ gatsby-wordpress-novela-theme/src/utils/handlers.js
@@ -12,6 +12,14 @@ function figcaption(h, node) {
   return h(node, "html", toHtml(node, { space: "html" }));
 }
 
+function video(h, node) {
+  return h(node, "html", toHtml(node, { space: "html" }));
+}
+
+function audio(h, node) {
+  return h(node, "html", toHtml(node, { space: "html" }));
+}
+
 function ul(h, node) {
   if (
     node.properties &&
@@ -38,4 +46,4 @@ function ul(h, node) {
   }
 }
 
-module.exports = { iframe, figcaption, ul };
+module.exports = { iframe, figcaption, ul, video, audio };
